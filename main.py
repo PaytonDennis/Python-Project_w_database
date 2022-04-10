bd45365793b251dd0a7ada28eeb0d755bb3449fe
PaytonDennis <102490243+PaytonDennis@users.noreply.github.com>
2022-04-10 02:26:27 -0500

    fixed error/ got debugger working

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 #import from innit 
 #runs flask application from webserver
 from distutils.log import debug
-from website import create_app
+from website.templates._init_ import create_app
 
 app = create_app()
 
 #if we run file execute line
-if __name__ == '_main_':
+if __name__ == '__main__':
     
     #start application, run webserver, when changes made rerun web server
     app.run(debug=True)
